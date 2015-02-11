37c707436fcf2ec0ed91a6f0c503865b3b82c6b0
wnalle <nalle@hawaii.edu>
2015-02-11 13:45:40 -1000

    final commit for WOD

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -1,9 +1,3 @@
-// This example is from _Java Examples in a Nutshell_. (http://www.oreilly.com)
-// Copyright (c) 1997 by David Flanagan
-// This example is provided WITHOUT ANY WARRANTY either expressed or implied.
-// You may study, use, modify, and distribute it for non-commercial purposes.
-// For any commercial use, see http://www.davidflanagan.com/javaexamples
-
 /**
  * This class defines methods for computing pseudo-random numbers, and it defines
  * the state variable that needs to be maintained for use by those methods.
@@ -11,31 +5,41 @@
 public class Randomizer {
     // Carefully chosen constants from the book "Numerical Recipes in C".
     // All "static final" fields are constants.
-    static final int m = 233280;  // the "modulus"
-    static final int a = 9301;    // the "multiplier"
-    static final int c = 49297;   // the "increment"
+    private static final int MOD = 233280;  // the "modulus"
+    private static final int MULT = 9301;    // the "multiplier"
+    private static final int INC = 49297;   // the "increment"
 
     // The state variable maintained by each Randomizer instance
-    long seed = 1;
+    private long seed = 1;
 
   /**
    * The constructor for the Randomizer() class.  It must be passed some
    * arbitrary initial value or "seed" for its pseudo-randomness.
+   *
+   * @param seed the seed value for the random generator
    **/
-  public Randomizer(long seed) { this.seed = seed; }
+  public Randomizer(long seed) {
+    this.seed = seed;
+  }
 
   /**
    * This method computes a pseudo-random number between 0 and 1 using a very
    * simple algorithm.  Math.random() and java.util.Random are actually a lot
    * better at computing randomness.
+   *
+   * @return a random float value.
    **/
   public float randomFloat() {
-      seed = (seed * a + c) % m; return (float)seed/(float)m;
+      seed = (seed * MULT + INC) % MOD; return (float) seed / (float) MOD;
   }
 
   /**
    * This method computes a pseudo-random integer between 0 and specified
    * maximum.  It uses randomFloat() above.
+   *
+   * @param max the maximum of the random value
+   * @return a random integer value between 0 and max
+   *
    **/
   public int randomInt(int max) {
       return Math.round(max * randomFloat());
@@ -44,11 +48,18 @@ public class Randomizer {
   /**
    * This nested class is a simple test program: it prints 10 random integers.
    * Note how the Randomizer object is seeded using the current time.
+   *
    **/
   public static class Test {
+    /**
+     * Generate 10 random values to test the Randomizer class.
+     * @param args ignored
+     */
       public static void main(String[] args) {
         Randomizer r = new Randomizer(new java.util.Date().getTime());
-        for(int i = 0; i < 10; i++) System.out.println(r.randomInt(100));
+        for (int i = 0; i < 10; i++) {
+          System.out.println(r.randomInt(100));
+        }
       }
   }
 }
---
+++ src/package-info.java
@@ -0,0 +1,3 @@
+/**
+ * Created by wgnalle on 2/11/2015.
+ */
